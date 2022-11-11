e89fa13835701f481f552eb6cb874abe4974286c
belar <bel.arth92@gmail.com>
2022-11-11 19:50:46 +0300

    Commit

---
+++ Example_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")  // .ToLower - приводит всё введённое к нижнему регистру 
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
